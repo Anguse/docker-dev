3c2b75cd939a4c759161ac40b7ffad61237988f1
Harald Lilja <harald.lilja@prevas.se>
2021-04-05 17:14:38 +0200

    Added build-run script

---
+++ build_run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker run --privileged -it --rm \
+	        -v `readlink -f /var/run/docker.sock`:/var/run/docker.sock \
+            dev-workspace \
+            tmux new
---
+++ get_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+
+# Docker CE for Linux installation script
+curl -fsSL https://get.docker.com -o get-docker.sh
+$ sh get-docker.sh
