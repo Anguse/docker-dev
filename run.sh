c50c5c7a5136b51bcb7a6557513ffe4e4dc26273
Anguse <liljaharald@gmail.com>
2022-01-15 19:14:28 +0100

    Updated run script to pull image from github registry
    * Removed Makefile

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 docker run --privileged -it --rm \
 	        -v `readlink -f /var/run/docker.sock`:/var/run/docker.sock \
-            base-dev \
+            -v ~/src:/home/raldo/src \
+            ghcr.io/anguse/docker-dev:main \
             tmux -u new
