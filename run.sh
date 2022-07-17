8efd0d3090063871897f47ddb2d0ce46a64e10cf
Anguse <liljaharald@gmail.com>
2022-07-17 21:04:35 +0200

    Updated run.sh
    Pull latest image

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+docker pull ghcr.io/anguse/docker-dev:main
+
 docker run --privileged -it --rm \
 	        -v `readlink -f /var/run/docker.sock`:/var/run/docker.sock \
             -v ~/src:/home/raldo/src \
+            -v ~/.ssh:/home/raldo/.ssh \
             -v /opt:/opt \
             ghcr.io/anguse/docker-dev:main \
             tmux -u new
